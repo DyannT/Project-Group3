0899de1e4ad8ae23469de5ef4833fc6d5e15e92a
Trường <99163832+DyannT@users.noreply.github.com>
2022-03-31 13:05:41 +0700

    Premisson change admin,test delete premisson admin

---
+++ assets/js/script-admin-about.js
@@ -0,0 +1,29 @@
+localStorage.getItem("content1About") && $(".text-shortly-about1").html(`${localStorage.getItem("content1About")}`)
+
+localStorage.getItem("content2About") && $(".text-shortly-about2").html(`${localStorage.getItem("content2About")}`)
+
+localStorage.getItem("content3About") && $(".text-author-inspiration1").html(`${localStorage.getItem("content3About")}`)
+
+localStorage.getItem("content4About") && $(".text-author-inspiration2").html(`${localStorage.getItem("content4About")}`)
+
+localStorage.getItem("content5About") && $(".text-author-inspiration3").html(`${localStorage.getItem("content5About")}`)
+
+localStorage.getItem("content6About") && $(".text-author-inspiration4").html(`${localStorage.getItem("content6About")}`)
+
+localStorage.getItem("content7About") && $(".testimonials-txt-title").html(`${localStorage.getItem("content7About")}`)
+
+localStorage.getItem("content8About") && $(".text-patrick-pool2").html(`${localStorage.getItem("content8About")}`)
+
+localStorage.getItem("content9About") && $(".list-1").html(`${localStorage.getItem("content9About")}`)
+
+localStorage.getItem("content10About") && $(".list-2").html(`${localStorage.getItem("content10About")}`)
+
+localStorage.getItem("content11About") && $(".list-3").html(`${localStorage.getItem("content11About")}`)
+
+localStorage.getItem("content12About") && $(".list-4").html(`${localStorage.getItem("content12About")}`)
+
+localStorage.getItem("content13About") && $(".list-5").html(`${localStorage.getItem("content13About")}`)
+
+localStorage.getItem("content14About") && $(".list-6").html(`${localStorage.getItem("content14About")}`)
+
+
---
+++ assets/js/script-admin-blog.js
@@ -0,0 +1,25 @@
+localStorage.getItem("content1Blog") && $(".txt-blog-page1").html(`${localStorage.getItem("content1Blog")}`)
+
+localStorage.getItem("content2Blog") && $(".txt-blog-page2").html(`${localStorage.getItem("content2Blog")}`)
+
+localStorage.getItem("content3Blog") && $(".txt-blog-page3").html(`${localStorage.getItem("content3Blog")}`)
+
+localStorage.getItem("content1BlogList") && $(".list-categories1").html(`${localStorage.getItem("content1BlogList")}`)
+
+localStorage.getItem("content2BlogList") && $(".list-categories2").html(`${localStorage.getItem("content2BlogList")}`)
+
+localStorage.getItem("content3BlogList") && $(".list-categories3").html(`${localStorage.getItem("content3BlogList")}`)
+
+localStorage.getItem("content4BlogList") && $(".list-categories4").html(`${localStorage.getItem("content4BlogList")}`)
+
+localStorage.getItem("content5BlogList") && $(".list-categories5").html(`${localStorage.getItem("content5BlogList")}`)
+
+localStorage.getItem("content6BlogList") && $(".list-categories6").html(`${localStorage.getItem("content6BlogList")}`)
+
+localStorage.getItem("content7BlogList") && $(".list-categories7").html(`${localStorage.getItem("content7BlogList")}`)
+
+localStorage.getItem("content4Blog") && $(".txt-recent-posts1").html(`${localStorage.getItem("content4Blog")}`)
+
+localStorage.getItem("content5Blog") && $(".txt-recent-posts2").html(`${localStorage.getItem("content5Blog")}`)
+
+localStorage.getItem("content6Blog") && $(".txt-recent-posts3").html(`${localStorage.getItem("content6Blog")}`)
