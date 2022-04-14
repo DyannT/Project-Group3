1ef45896ed9617c2c385cd150adec4e755b13983
Trường <99163832+DyannT@users.noreply.github.com>
2022-04-14 19:55:13 +0700

    ok

--- assets/js/script-login.js
+++ assets/js/script-login.js
@@ -44,25 +44,6 @@ const loading = document.querySelector('.loading')
 
 
 
-
-
-// console.log(modal)
-// console.log(loGin)
-// console.log(closebtn)
-// console.log(userName)
-// console.log(password)
-// console.log(loginAdmin)
-// console.log(formLogin.action)
-// console.log(backbtn)
-// console.log(creaTe)
-// console.log(modalCreate)
-// console.log(closebtn1)
-// console.log(createAcc)
-// console.log(loginText)
-// console.log(accountText)
-
-
-// Change color text
 function generateColor() {
   let color;
   let r = Math.floor(Math.random() * 256)
@@ -98,37 +79,6 @@ closebtn1.onclick = function () {
 
 
 
-// //////////Test
-// console.log(!(userName.value == "admin"))
-// console.log(password.value = "123456")
-
-// function checkLogin(){
-//     if(!(userName.value == "admin") || !!(password.value == "123456")){
-//         return false
-//     }
-//     else if((userName.value == "admin") && (password.value == "123456")){
-//         return true
-//     }
-// }
-
-// if(loginAdmin.onclick === checkLogin())
-// {
-//     console.log("Đăng nhập thành công!!")
-// }
-
-
-
-
-
-
-
-// // Check register
-
-// console.log(localStorage.getItem("create-user-form"))
-// console.log(localStorage.getItem("create-password-form"))
-
-// $("#title_hello").html(`Tôi là ${localStorage.getItem("nameUser")}`)
-
 var isRegister = false;
 var code = '205106';
 storageUsername = []
@@ -136,27 +86,10 @@ storagePassword = []
 storageUsernameCheck = []
 storagePasswordCheck = []
 
-// console.log(checkCode === parseInt(code))
+
 
 createAcc.onclick = function (e) {
-  // Cách 1 lưu qua data tạm thời khi refresh sẽ mất
-
-
-  // e.preventDefault()
-  // var createUser = userNameCreate.value;
-  // // console.log(createUser)
-  // var createPassword = passWordCreate.value;
-  // var user1 = new createData(createUser,createPassword)
-  // if(checkCreate(user1.username,user1.password) && user1.username !== '' && user1.username){
-  //   isRegister = true;
-  //   data.push(user1);
-  //   // console.log(data)
-  //   actionCreate(e)
-  // }
-  // else{
-  //   alert("Tài khoản hoặc mật khẩu bị trùng!!!")
-  //   e.preventDefault()
-  // }
+  
 
 
   // Cách 2 lưu qua localStorage của web khi refresh sẽ k mất như k tạo đc nhiều acc và chỉ dùng đc ở web của ng dùng
@@ -218,21 +151,13 @@ function checkCreate(username,password,storageUsernameCheck,storagePasswordCheck
 }
 
 
-// function checkRepeat() {
-//   console.log(data1[username])
-// }
-
-// alert(data1.concat(data))
 
 function actionCreate(e){
   if(isRegister){
     alert("Đăng ký thành công!!!")
     e.preventDefault()
   }
-  // else{
-  //   // alert("Tài khoản hoặc mật khẩu bị trùng!!!")
-  //   e.preventDefault()
-  // }
+
 }
 
 
